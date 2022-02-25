bd0693803993ca442b5a03a71b1783f8e09d3eac
jjjadey <nattakan.premp@gmail.com>
2022-02-25 10:20:49 +0700

    Display products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,36 @@
 import React from 'react'
+// import { useSelector } from 'react-redux';
 import { useGetAllProductsQuery } from '../features/productApi';
 
 const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
   console.log(data, error, isLoading);
 
+  // const { items, status } = useSelector(state => state.products);
+  // console.log('>> slice', items, status);
+
   return (
     <div className="home-container">
-      <>
-        <h2>New Arrivals</h2>
-        <div className="products">
-          <div className="product">
-            <h3>product name</h3>
-            img
-            {/* <img /> */}
-            <div className="details">
-              <span>description</span>
-              <span className="price">10</span>
+      {isLoading ?
+        <p>Loading..</p> : error ?
+          <p>{error.status} | {error.error}</p> :
+          <>
+            <h2>New Arrivals</h2>
+            <div className="products">
+              {data?.map(product => (
+                <div key={product.id} className="product">
+                  <h3>{product.name}</h3>
+                  <img src={product.image} alt="" />
+                  <div className="details">
+                    <span>{product.desc}</span>
+                    <span className="price">${product.price}</span>
+                  </div>
+                  <button > Add To Cart </button>
+                </div>
+              ))}
             </div>
-            <button > Add To Cart </button>
-          </div>
-        </div>
-      </>
-
+          </>
+      }
     </div>
   );
 }
