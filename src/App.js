f49103f298a9bff8e02788943018de0faa5ef8c5
jjjadey <nattakan.premp@gmail.com>
2022-03-09 20:45:08 +0700

    get random unsplash images and render image components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import Loader from './Loader';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import axios from 'axios';
+import UnsplashImage from './UnsplashImage';
+
+//styles
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    font-family: sans-serif;
+  }
+`;
+
+const WrapperImages = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px,1fr));
+  grid-auto-rows: 300px;
+`;
+
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const apiRoot = "https://api.unsplash.com/";
+    const accessKeyUnsplash = process.env.REACT_APP_ACCESS_KEY_UNSPLASH;
+    const count = 10;
+    axios.get(`${apiRoot}/photos/random?client_id=${accessKeyUnsplash}&count=${count}`)
+      .then(res => {
+        setImages([...images, ...res.data]);
+      });
+
+    console.log(images)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <GlobalStyle />
+      <Loader />
+      <WrapperImages>
+        {images.map((image, index) => (
+          <UnsplashImage url={image.urls.thumb} key={`${image.id}${index}`} />
+        ))}
+      </WrapperImages>
+
     </div>
   );
 }
