dbce092dffd90fc9b1a914cdb6838898131ce658
jjjadey <nattakan.premp@gmail.com>
2022-03-09 20:51:45 +0700

    Header and loader component

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.header`
+  max-width: 70rem;
+  margin: 2rem auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Oswald',sans-serif;
+  margin-bottom: 1em;
+`;
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Heading>
+      <H1>Infinite Scroll Image Gallery</H1>
+      <p>Using React and Unsplash</p>
+    </Heading>
   )
 }
 
