575c6e5dbfb9c7c0dabd581dcc2a3d5824e0698b
jjjadey <nattakan.premp@gmail.com>
2022-07-30 17:26:21 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+//pages
+import HomePage from './pages/HomePage';
+import SignUp from './pages/SignupPage';
+import Pricing from './pages/PricingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
   );
 }
 
