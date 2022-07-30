86c50794c27342c1409d0d5cdf5e1a4ae6203e60
jjjadey <nattakan.premp@gmail.com>
2022-07-30 18:57:20 +0700

    Content section for home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import Features from '../components/features/Features'
-import Hero from '../components/hero/Hero'
+import React from 'react';
+import { Content } from '../components/content/Content';
+import Features from '../components/features/Features';
+import Hero from '../components/hero/Hero';
+import { contentOne, contentTwo, contentThree } from '../data/ContentData';
 
 const HomePage = () => {
     return (
         <>
-        <Hero />
-        <Features/>
+            <Hero />
+            <Features />
+            <Content {...contentOne} />
+			<Content {...contentTwo} />
+			<Content {...contentThree} />
         </>
-        
-
     )
 }
 
